8dfca3ed9c6c9d015802a5eba718a4805e8a785e
x <x@washswat.com>
2021-03-04 17:14:46 +0900

    operation

--- initial/regexr.js
+++ initial/regexr.js
@@ -1,7 +1,35 @@
 class _reg {
 	constructor() {
-		console.log("start")
+		this.op();
 	}
+	op = () => {
+		const _string =
+			'Hi there, Nice to meet you. And Hello there and hi.\n' +
+			'I love grey(gray) color not a gry, graay and graaay.\n' +
+			'Ya ya YaYaYa Ya\n' +
+			'\n' +
+			'abcdefghijklmnopqrstuvwxyz\n' +
+			'ABSCEFGHIJKLMNOPQRSTUVWZYZ\n' +
+			'1234567890\n' +
+			'\n' +
+			'.[]{}()\\^$|?*+\n' +
+			'\n' +
+			'010-898-0893\n' +
+			'010-405-3412\n' +
+			'02-878-8888\n' +
+			'\n' +
+			'dream.coder.ellie@gmail.com\n' +
+			'hello@daum.net\n' +
+			'hello@daum.co.kr\n' +
+			'\n' +
+			'https://www.youtu.be/-ZClicWm0zM\n' +
+			'https://youtu.be/-ZClicWm0zM\n' +
+			'youtu.be/-ZClicWm0zM';
+
+		const regYtId = /(?:https?:\/\/)?(?:www\.)?youtu.be\/([a-zA-Z0-9-]{11})/gm;
+		const confirm = _string.match(regYtId);
+		console.log('========', confirm);
+	};
 }
 
-new _reg()
+const reg = new _reg();
